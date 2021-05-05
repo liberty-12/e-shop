572176e97b34fda9e30c0b30340bc15cbbb3306d
Kortoshkina Ekaterina <liberty_12@mail.ru>
2021-05-05 18:01:48 +0200

    initial commit
    upload images, fonts, css, js, basic pages layout (index, catalog)

---
+++ project/catalog.php
@@ -0,0 +1,201 @@
+<?php require __DIR__ . '/includes/header.php' ?>
+
+
+  <main class="main">
+    <div class="main-container">
+
+      <h1 class="main-heading">Products</h1>
+      <section class="breadcrumbs">
+        <h2 class="visually-hidden">Navigation</h2>
+        <ul class="breadcrumbs-list">
+          <li class="breadcrumbs-item"><a class="breadcrumbs-link" href="index.html">Main</a></li>
+          <li class="breadcrumbs-item"><a class="breadcrumbs-link breadcrumbs-current" href="#">Products</a></li>
+        </ul>
+      </section>
+
+    </div>
+
+    <div class="main-content-container">
+
+      <div class="catalog-headers">
+        <div class="main-container">
+
+          <h2 class="filter-header">Filters:</h2>
+          <section class="sort">
+            <h2>Sort by:</h2>
+            <ul class="sort-list">
+              <li class="sort-item current-sort-item"><a class="sort-item-link" href="#">Price</a></li>
+              <li class="sort-item"><a class="sort-item-link" href="#">Newest</a></li>
+              <li class="sort-item"><a class="sort-item-link" href="#">Popular</a></li>
+            </ul>
+            <ul class="order-list">
+              <li class="order-item order-item-up">
+                <a href="#" class="order-item-link" aria-label="Ascending"></a>
+              </li>
+              <li class="order-item order-item-down">
+                <a href="#" class="order-item-link" aria-label="Descending"></a>
+              </li>
+            </ul>
+          </section>
+
+        </div>
+      </div>
+
+
+      <div class="catalog-page-content">
+        <div class="main-container">
+
+          <aside class="form">
+            <form class="filter" method="post" action="">
+              <div class="filter-block range-div">
+                <label>Price</label>
+                <div class="filter-range">
+                  <button type="button" class="range-button range-left" aria-label="Minimal price"></button>
+                  <button type="button" class="range-button range-right" aria-label="Maximum price"></button>
+                  <div class="range-field"></div>
+                </div>
+                <span>from 0</span>
+                <span>to 5000</span>
+              </div>
+              <fieldset class="filter-block color">
+                <legend>Color</legend>
+                <ul class="filter-list">
+                  <li class="filter-option">
+                    <input class="filter-input filter-input-checkbox visually-hidden" type="checkbox" name="black" id="filter-black" checked>
+                    <label for="filter-black">Black</label>
+                  </li>
+                  <li class="filter-option">
+                    <input class="filter-input filter-input-checkbox visually-hidden" type="checkbox" name="white" id="filter-white">
+                    <label for="filter-white">White</label>
+                  </li>
+                  <li class="filter-option">
+                    <input class="filter-input filter-input-checkbox visually-hidden" type="checkbox" name="blue" id="filter-blue" checked>
+                    <label for="filter-blue">Blue</label>
+                  </li>
+                  <li class="filter-option">
+                    <input class="filter-input filter-input-checkbox visually-hidden" type="checkbox" name="red" id="filter-red" checked>
+                    <label for="filter-red">Red</label>
+                  </li>
+                  <li class="filter-option">
+                    <input class="filter-input filter-input-checkbox visually-hidden" type="checkbox" name="pink" id="filter-pink">
+                    <label for="filter-pink">Pink</label>
+                  </li>
+                </ul>
+              </fieldset>
+              <fieldset class="filter-block bluetooth">
+                <legend>Bluetooth</legend>
+                <ul class="filter-list">
+                  <li class="filter-option">
+                    <input class="filter-input filter-input-radio visually-hidden" type="radio" name="radio" id="filter-true" checked>
+                    <label for="filter-true">Yes</label>
+                  </li>
+                  <li class="filter-option">
+                    <input class="filter-input filter-input-radio visually-hidden" type="radio" name="radio" id="filter-false">
+                    <label for="filter-false">No</label>
+                  </li>
+                </ul>
+              </fieldset>
+              <button class="button filter-button" type="submit">Filter</button>
+            </form>
+          </aside>
+
+          <div class="content-container">
+            <section class="catalog">
+              <h2 class="visually-hidden">Products</h2>
+              <ul class="catalog-list">
+                <li class="catalog-item">
+                  <article class="catalog-item-block">
+                    <a class="catalog-item-link" href="#">
+                      <img src="img/item-1.jpg" width="360" height="380" alt="Любительская селфи-палка">
+                    </a>
+                    <div class="catalog-description-wrapper">
+                      <h1 class="catalog-item-description"><a href="#">Любительская селфи-палка</a></h1>
+                      <span class="catalog-item-price">500 руб.</span>
+                    </div>
+                    <div class="catalog-item-hover">
+                      <a class="button catalog-button" href="#">В корзину</a>
+                      <a class="catalog-compare-link" href="#">Добавить к сравнению</a>
+                    </div>
+                  </article>
+                </li>
+
+                <li class="catalog-item">
+                  <article class="catalog-item-block">
+                    <a class="catalog-item-link" href="#">
+                      <img src="img/item-2.jpg" width="360" height="380" alt="Профессиональная селфи-палка">
+                    </a>
+                    <div class="catalog-description-wrapper">
+                      <h1 class="catalog-item-description"><a href="#">Профессиональная селфи-палка</a></h1>
+                      <span class="catalog-item-price">1 500 руб.</span>
+                    </div>
+                    <div class="catalog-item-hover">
+                      <a class="button catalog-button" href="#">В корзину</a>
+                      <a class="catalog-compare-link" href="#">Добавить к сравнению</a>
+                    </div>
+                  </article>
+                </li>
+
+                <li class="catalog-item">
+                  <article class="catalog-item-block">
+                    <a class="catalog-item-link" href="#">
+                      <img src="img/item-3.jpg" width="360" height="380" alt="Непотопляемая селфи-палка">
+                    </a>
+                    <div class="catalog-description-wrapper">
+                      <h1 class="catalog-item-description"><a href="#">Непотопляемая селфи-палка</a></h1>
+                      <span class="catalog-item-price">2 500 руб.</span>
+                    </div>
+                    <div class="catalog-item-hover">
+                      <a class="button catalog-button" href="#">В корзину</a>
+                      <a class="catalog-compare-link" href="#">Добавить к сравнению</a>
+                    </div>
+                  </article>
+                </li>
+
+                <li class="catalog-item">
+                  <article class="catalog-item-block">
+                    <a class="catalog-item-link" href="#">
+                      <img src="img/item-4.jpg" width="360" height="380" alt="Селфи-палка &laquo;Следуй за мной&raquo;">
+                      <span class="catalog-item-new">New</span>
+                    </a>
+                    <div class="catalog-description-wrapper">
+                      <h1 class="catalog-item-description"><a href="#">Селфи-палка &laquo;Следуй за мной&raquo;</a></h1>
+                      <span class="catalog-item-price">4 900 руб.</span>
+                    </div>
+                    <div class="catalog-item-hover">
+                      <a class="button catalog-button" href="#">В корзину</a>
+                      <a class="catalog-compare-link" href="#">Добавить к сравнению</a>
+                    </div>
+                  </article>
+                </li>
+              </ul>
+            </section>
+
+            <section class="pages">
+              <h2 class="visually-hidden">Pages</h2>
+              <ul class="pages-list">
+                <li class="pages-item">
+                  <a class="pages-item-link">Back</a>
+                </li>
+                <li class="pages-item">
+                  <a class="pages-item-link current-page">1</a>
+                </li>
+                <li class="pages-item">
+                  <a class="pages-item-link" href="#">2</a>
+                </li>
+                <li class="pages-item">
+                  <a class="pages-item-link" href="#">3</a>
+                </li>
+                <li class="pages-item">
+                  <a class="pages-item-link" href="#">Next</a>
+                </li>
+              </ul>
+            </section>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  </main>
+
+  <?php require __DIR__ . '/includes/footer.php' ?>
---
+++ project/includes/popups.php
@@ -0,0 +1,31 @@
+<section class="map-popup hidden">
+    <h2 class="visually-hidden">Map</h2>
+    <iframe class="map-iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2249.1125488779985!2d37.527429315579646!3d55.6870309805358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54cf65f5c8955%3A0x694710ccd55501e!2z0YPQuy4g0KHRgtGA0L7QuNGC0LXQu9C10LksIDE1LCDQnNC-0YHQutCy0LAsIDExOTMxMQ!5e0!3m2!1sru!2sru!4v1548171893150"
+        width="560" height="222" allowfullscreen></iframe>
+    <button class="close-button" type="button" aria-label="Close map"></button>
+  </section>
+
+  <section class="contact-popup hidden">
+    <h2 class="visually-hidden">Contact us</h2>
+    <form class="contact-popup-form" method="post" action="">
+      <button class="close-button" type="button" aria-label="Close form"></button>
+
+      <div class="contact-popup-container">
+        <p class="contact-popup-input">
+          <label for="name">Full name:</label>
+          <input type="text" name="name" id="name" placeholder="Full name" />
+        </p>
+
+        <p class="contact-popup-input">
+          <label for="email">E-mail:</label>
+          <input type="email" name="email" id="email" placeholder="email@example.com" />
+        </p>
+      </div>
+
+      <label for="text">Comment</label>
+      <textarea name="text" id="text" placeholder="Write your comment"></textarea>
+
+      <button class="button popup-button" type="submit">Send</button>
+    </form>
+  </section>
+
